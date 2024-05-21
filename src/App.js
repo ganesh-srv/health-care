e9d9a440e119db0580f642d9e110b5ebdd76e09e
ganesh-srv <ganeshsrv.sarakadam@gmail.com>
2024-05-20 22:10:45 -0400

    layout css done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
-
+import PatientsCard from './components/PatientsCard/PatientCard';
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='container'>
+        <div className='patients-listing '>
+
+        </div>
+        <div className='diagnosis'>
+
+        </div>
+        <div className='profile-card'>
+
+        </div>
+      </div>
     </div>
   );
 }
